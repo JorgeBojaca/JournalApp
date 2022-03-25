8042ddf2606fe14e75c8d69f0ae0eddbe8cee0a2
Jorge Bojacá <jorgebojaca@gmail.com>
2022-03-25 13:59:01 -0500

    Se remueven propiedades y state no utilizado

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { AppRouter } from './componets/routers/AppRouter';
 import { store } from './store/store';
 
 export const JournalApp = () => {
-  const [login, setLogin] = useState(false);
   return (
     <Provider store={store}>
-      <AppRouter login={login} setLogin={setLogin} />
+      <AppRouter />
     </Provider>
   );
 };
