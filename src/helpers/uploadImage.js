b154692913d2c6f047ea04b3beb0cf9ba5c8265d
Jorge Bojacá <jorgebojaca@gmail.com>
2022-03-30 01:01:19 -0500

    Se agrega validación y manejo de error

--- src/helpers/uploadImage.js
+++ src/helpers/uploadImage.js
@@ -1,13 +1,19 @@
 const uploadImage = async (file) => {
+  if (file.size === 0) return null;
+
   const formData = new FormData();
   formData.append('file', file);
   formData.append('upload_preset', 'react-journal');
-  const res = await fetch(
-    'https://api.cloudinary.com/v1_1/dzavnsubr/image/upload',
-    { method: 'POST', body: formData }
-  );
-  const jsonRes = await res.json();
-  return jsonRes.secure_url;
+  try {
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/dzavnsubr/image/upload',
+      { method: 'POST', body: formData }
+    );
+    const jsonRes = await res.json();
+    return jsonRes.secure_url;
+  } catch (error) {
+    return null;
+  }
 };
 
 export default uploadImage;
